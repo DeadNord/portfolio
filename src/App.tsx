f8e5b3eb5b924013a0304df73e0fea3bbf564c50
Yegor Osipov <83457266+DeadNord@users.noreply.github.com>
2023-11-13 19:11:56 +0100

    fix App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import Container from './components/shared/container/Container';
 import Education from './components/widgets/education/Education';
 import Info from './components/widgets/info/Info';
 import Projects from './components/widgets/projects/Projects';
 import Stack from './components/widgets/stack/Stack';
-import Works from './components/widgets/works/Works';
 
 function App() {
   return (
     <>
       {/* <Info /> */}
-      <Stack />
-      {/* <Education /> */}
+      {/* <Stack /> */}
+      <Education />
       {/* <Projects /> */}
     </>
   );
